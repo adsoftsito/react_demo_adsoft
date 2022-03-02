73d864175168be90b91c18521910d40aef7b5cad
adsoftsito <adsoft@live.com.mx>
2022-03-02 10:25:12 -0600

    update addition Closes #5

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Demo calc
+          Demo calc - adsoft
         </p>
         
        <Addition />
